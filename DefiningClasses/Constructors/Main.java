0be29fc6d72bc25a3e4cf9bd98f837e7d6031382
Majesticate <81169304+Majesticate@users.noreply.github.com>
2022-01-29 09:38:20 +0200

    Add files via upload

---
+++ DefiningClasses/Constructors/Main.java
@@ -0,0 +1,29 @@
+package CarConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<Car> listOfCars = new ArrayList<>();
+
+        while (n-- > 0) {
+            String[] tokens = scanner.nextLine().split("\\s+");
+
+            Car newCar;
+            if (tokens.length == 1) {
+                newCar = new Car(tokens[0]);
+            } else {
+                newCar = new Car(tokens[0], tokens[1], Integer.parseInt(tokens[2]));
+            }
+            listOfCars.add(newCar);
+        }
+
+        listOfCars.forEach(e ->e.carInfo());
+    }
+}
