455fe6494d338151f28f180923074e8e223bd03c
Ivan <ivan4oto22@abv.bg>
2022-02-04 19:33:05 +0200

    UpdateClass

---
+++ DefiningClasses/CarConstructors/Car.java
@@ -0,0 +1,45 @@
+package CarConstructor;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int hp;
+
+    public Car(String brand, String model, int hp) {
+        this.brand = brand;
+        this.model = model;
+        this.hp = hp;
+    }
+
+    public Car(String brand) {
+        this(brand, "unknown", -1);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void carInfo() {
+        System.out.println(String.format("The car is: %s %s - %d HP.", this.brand, this.model, this.hp));
+    }
+}
--- DefiningClasses/CarInfo/Main.java
+++ DefiningClasses/CarInfo/Main.java
@@ -1,4 +1,4 @@
-package CarInfo;
+package CarConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
